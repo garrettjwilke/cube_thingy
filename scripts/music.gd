94aa898a78e793dfd4b4b0d66ed6eaf1313edcba
homeless <homeless@homelesss-MacBook-Pro.local>
2024-07-24 23:43:56 -0600

    added test music

--- scripts/music.gd
+++ scripts/music.gd
@@ -2,13 +2,37 @@ extends Node
 
 @onready var player = $AudioStreamPlayer2D
 
+var song
+var song_number = 0
+func load_song():
+	song_number += 1
+	var song_string = str("res://assets/music/loop_",song_number,".mp3")
+	if not FileAccess.file_exists(song_string):
+		song_number = 0
+		load_song()
+		return
+	else:
+		song = load(song_string)
+
 func music_looper():
 	if GLOBALS.MUTE_MUSIC:
+		if player.playing:
+			player.stop()
 		return
-	if player.playing == true:
+	if player.playing:
 		return
+	player.stream = song
+	player.volume_db = 0
 	player.play()
 
+func _ready():
+	load_song()
+
+var TIMER = 0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _process(delta):
+	TIMER += delta
+	if TIMER > 30:
+		load_song()
+		TIMER = 0
 	music_looper()
